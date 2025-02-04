6ba6418741a2c3f95e0c18ccc572d0e490063cd7
Dylan Lukes <lukes.dylan@gmail.com>
2025-02-04 00:50:56 -0800

    refine tests

--- observables/src/test/java/edu/ucsd/cse110/observables/TransformationsTests.java
+++ observables/src/test/java/edu/ucsd/cse110/observables/TransformationsTests.java
@@ -1,6 +1,7 @@
 package edu.ucsd.cse110.observables;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
@@ -9,14 +10,18 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * These tests aren't written in BDD style like the others.
  * They don't fit it as well as they're not testing objects but
  * functions that exist outside any particular objects.
  */
+
+@DisplayName("Transformations")
 public class TransformationsTests {
     @Test
+    @DisplayName("mapping (map)")
     void testMap() {
         var input = new PlainMutableSubject<String>();
 
@@ -36,6 +41,7 @@ public class TransformationsTests {
     }
 
     @Test
+    @DisplayName("switching (switchMap)")
     void testSwitchMap() {
         var left = new PlainMutableSubject<String>();
         var right = new PlainMutableSubject<String>();
@@ -43,26 +49,55 @@ public class TransformationsTests {
 
         var output = Transformations.switchMap(trigger, b -> b ? right : left);
 
-        var latch = new CountDownLatch(2);
+        var leftLatch = new CountDownLatch(1);
+        var rightLatch = new CountDownLatch(1);
+        var outputLatch = new CountDownLatch(2);
 
+        // Expects just "SENT TO LEFT"
         left.observe(value -> {
             assertThat(value, is("SENT TO LEFT"));
-            latch.countDown();
+            leftLatch.countDown();
         });
 
+        // Expects just "SENT TO RIGHT"
         right.observe(value -> {
             assertThat(value, is("SENT TO RIGHT"));
-            latch.countDown();
+            rightLatch.countDown();
+        });
+
+        // Expects to get forwarded one and then the other
+        output.observe(value -> {
+            switch ((int) outputLatch.getCount()) {
+                case 2: // first
+                    assertThat(value, is("SENT TO LEFT"));
+                    break;
+                case 1: // second
+                    assertThat(value, is("SENT TO RIGHT"));
+                    break;
+                default: // never!
+                    fail();
+            }
+            outputLatch.countDown(); // 2 -> 1 -> 0
         });
 
         assertDoesNotThrow(() -> {
             trigger.setValue(false);
             left.setValue("SENT TO LEFT");
+            leftLatch.await(1, TimeUnit.SECONDS);
+            assertThat(output.getValue(), is("SENT TO LEFT"));
 
             trigger.setValue(true);
             right.setValue("SENT TO RIGHT");
+            rightLatch.await(1, TimeUnit.SECONDS);
+            assertThat(output.getValue(), is("SENT TO RIGHT"));
 
-            latch.await(1, TimeUnit.SECONDS);
+            // Wait for everything to settle.
+            outputLatch.await(1, TimeUnit.SECONDS);
+
+            // All have counted down fully.
+            assertThat(leftLatch.getCount(), is(0L));
+            assertThat(rightLatch.getCount(), is(0L));
+            assertThat(outputLatch.getCount(), is(0L));
         });
     }
 
