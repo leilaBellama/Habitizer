2f89c902a4d2207db556c7589fe3f137741b5298
lmbellama <130100171+lmbellama@users.noreply.github.com>
2025-02-13 01:39:36 -0800

    simplified evening/morning

--- lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/MorningEveningTasklistTest.java
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/MorningEveningTasklistTest.java
@@ -26,37 +26,4 @@ public class MorningEveningTasklistTest {
         assertEquals((int) numTaskAfterRemove, 0);
     }
 
-
-/*
-    @Test
-    public void testFindMorning(){
-        var task1 = new Task(1, "new task morning",true);
-        taskRepository.saveMorning(task1);
-
-        assertEquals(taskRepository.findMorning(1).getValue(), task1);
-    }
-
-    @Test
-    public void testSaveEvening(){
-        var numTaskMorning = taskRepository.countMorning();
-        var numTask = taskRepository.countEvening();
-        var task1 = new Task(4, "new task evening",false);
-        taskRepository.saveEvening(task1);
-        var numTaskAfter = taskRepository.countEvening();
-        var numTaskMorningAfter = taskRepository.countMorning();
-
-        assertEquals((int) numTaskAfter, numTask + 1);
-        assertEquals((int) numTaskMorningAfter, (int) numTaskMorning);
-    }
-
-    @Test
-    public void testFindEvening(){
-        var task1 = new Task(1, "new task evening",false);
-        taskRepository.saveEvening(task1);
-
-        assertEquals(taskRepository.findEvening(1).getValue(), task1);
-    }
-
- */
-
 }
